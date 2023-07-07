8f0b96f7c4f50f1977ca073533cb6474ae58b681
fededav <67575679+raishid@users.noreply.github.com>
2023-07-07 16:02:24 -0400

    update cookie concent

--- script.js
+++ script.js
@@ -1,3 +1,83 @@
+const secctionDetails = `
+    <div class="container-detail">
+        <div class="detail">
+            <div>
+                <span class="subtitle-details">${Cookiebot.dialog.cookieHeaderTypeNecessary.replace('({0})', '')} </span>
+                <span class="count-detail">${Cookiebot.dialog.cookieTableNecessary.length}</span>
+            </div>
+            <div>
+                <label class="switch">
+                    <input type="checkbox" disabled checked>
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>
+        <div class="detail">
+            <div>
+                <span class="subtitle-details">${Cookiebot.dialog.cookieHeaderTypePreference.replace('({0})', '')} </span>
+                <span class="count-detail">${Cookiebot.dialog.cookieTablePreference.length}</span>
+            </div>
+            <div>
+                <label class="switch">
+                    <input type="checkbox" checked>
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>
+        <div class="detail">
+            <div>
+                <span class="subtitle-details">${Cookiebot.dialog.cookieHeaderTypeStatistics.replace('({0})', '')} </span>
+                <span class="count-detail">${Cookiebot.dialog.cookieTableStatistics.length}</span>
+            </div>
+            <div>
+                <label class="switch">
+                    <input type="checkbox" checked>
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>
+    </div>
+`;
+
+const aboutCookies = `<div class="container-about-cookie">
+    <p>
+        ${Cookiebot.dialog.cookieIntroText}
+    </p>
+</div>`
+
+const templateDetails = `<nav class="nav-dropdown">
+        <ul>
+            <li class="active" onclick="showDetails(this)">${Cookiebot.dialog.details}</li>
+            <li onclick="showAboutCookies(this)">${Cookiebot.dialog.aboutCookiesText}</li>
+        </ul>
+    </nav>
+    <section class="content-details">${secctionDetails}</section>
+`;
+
+function showDetails(e) {
+    document.querySelector('.nav-dropdown li.active').classList.remove('active');
+    e.classList.add('active');
+    document.querySelector('#cookiebanner').classList.remove('active-text');
+    document.querySelector("#cookiebanner .content-details").innerHTML = secctionDetails;
+}
+
+function showAboutCookies(e) {
+    document.querySelector('.nav-dropdown li.active').classList.remove('active');
+    e.classList.add('active');
+    document.querySelector('#cookiebanner').classList.add('active-text');
+    document.querySelector("#cookiebanner .content-details").innerHTML = aboutCookies;
+}
+
+function addDropdown() {
+    const bt = document.querySelector('#manageCookie');
+    bt.innerHTML = Cookiebot.dialog.loiAllowSelectionText
+    bt.attributes.onclick.value = 'Cookiebot.dialog.submitConsent()';
+    bt.style.fontSize = '14px';
+    document.querySelector("#cookiebanner .container-first").innerHTML = templateDetails;
+}
+
+console.log(Cookiebot.dialog);
+
 var cookieBannerSliderPos = 0;
 
 function showCookieBanner() {
